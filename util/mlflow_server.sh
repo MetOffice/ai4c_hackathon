a556eb141d6a1d9d831775f6972c1ab3c5da5082
Stephen Haddad <stephen.haddad@metoffice.gov.uk>
2026-02-27 08:33:27 +0000

    added mlflow server

--- util/mlflow_server.sh
+++ util/mlflow_server.sh
@@ -6,13 +6,16 @@
 # - This script should be run in a terminal on  server where you are doing the training. You should point to a conda environment for that server.
 # Further information on running ML flow can be found in the docs  https://mlflow.org/docs/latest/ml/ 
 
-#MLFLOW_ROOT = ""
+export ENV_TOOL=${1:-venv} # can be venv or conda
+export MLFLOW_ENV=${2:-~/venv/ai4c_nb_cpu}
 export MLFLOW_ROOT=${3:-$HOME/mlflow}
-export MLFLOW_PORT=${2:-4455}
-export MLFLOW_ENV=${1:-~/venv/ai4c_nb_cpu}
+export MLFLOW_PORT=${4:-4455}
 
-# conda activate ${MLFLOW_ENV}
-. ${MLFLOW_ENV}/bin/activate
+if [ $ENV_TOOL = "conda" ]; then
+    conda activate ${MLFLOW_ENV}
+else
+    . ${MLFLOW_ENV}/bin/activate
+fi
 
 echo starting MLflow server  "MLFLOW_ROOT: ${MLFLOW_ROOT}  MLFLOW_PORT: ${MLFLOW_PORT}"
 mlflow server --backend-store-uri "sqlite:////${MLFLOW_ROOT}/backend.db" --artifacts-destination ${MLFLOW_ROOT}/artifacts/ --registry-store-uri ${MLFLOW_ROOT}/registry/ --port ${MLFLOW_PORT}
